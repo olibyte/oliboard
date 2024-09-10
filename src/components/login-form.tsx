7367d2406b046483b4c0bc3be34cf66e0e4aae28
olibyte <ocben1@gmail.com>
2024-09-10 10:33:30 +1000

    feat: add signup route

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -51,7 +51,7 @@ export function LoginForm() {
         </div>
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
+          <Link href="/signup" className="underline">
             Sign up
           </Link>
         </div>
